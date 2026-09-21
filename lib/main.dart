275863390de6f3ef8953286008a34aad42e7c74e
Hagar Elganzory <hagarelganzory51@gmail.com>
2026-09-22 02:45:19 +0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'package:maps_app/home_screen.dart';
 void main() {
   runApp(const MainApp());
 }
-//key=
-//AIzaSyDmFMCSA4aA2n9y1I-HcDpR9kR7WZyb6P4
 
-//AIzaSyCrVKch8-VV5AdTrKhpS6h9Nr_jRwnlHKo
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
